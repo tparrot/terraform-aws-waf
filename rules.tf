9dd628315d290be47b8a2f7aa859a53e86920883
Tim Meurer <Tim.Meurer@infraview.net>
2025-12-15 16:32:16 +0100

    feature: adds challenge request action to rate based rules

--- rules.tf
+++ rules.tf
@@ -2129,6 +2129,11 @@ resource "aws_wafv2_web_acl" "default" {
           for_each = rule.value.action == "captcha" ? [1] : []
           content {}
         }
+        # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl#challenge-block
+        dynamic "challenge" {
+          for_each = rule.value.action == "challenge" ? [1] : []
+          content {}
+        }
       }
 
       statement {
